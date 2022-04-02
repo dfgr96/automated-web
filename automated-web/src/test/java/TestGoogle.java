b2773991dd0b686f67725a15a09001b12486e1f7
Felipe Gomez Rodriguez <davidfelgomezr@hotmail.com>
2022-04-02 15:19:34 -0500

    First commit automation web with selenium

---
+++ automated-web/src/test/java/TestGoogle.java
@@ -0,0 +1,34 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestGoogle {
+
+    public WebDriver driver;
+    public FindElements findElements;
+    public String baseUrl;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+        baseUrl = "https://google.com/";
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testGoogleSearch() throws IOException, ParseException {
+        Object ob = new JSONParser().parse(new FileReader("src/test/resources/datos.json"));
+        JSONObject js = (JSONObject) ob;
+        findElements = new FindElements(driver);
+        findElements.setInputGoogle(js.get("firstName").toString());
+        //driver.quit();
+    }
+}
