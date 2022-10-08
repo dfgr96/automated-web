dc26508fa927cb52958d4d0681df1442275605c0
dfgr96 <davidfelgomezr@hotmail.com>
2022-10-07 21:49:38 -0500

    Add new chromedriver and test in string

--- src/test/java/TestGoogle.java
+++ src/test/java/TestGoogle.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -15,8 +16,8 @@ public class TestGoogle {
     public String baseUrl;
 
     @BeforeMethod
-    public void beforeMethod(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+    public void beforeMethod() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
         baseUrl = "https://google.com/";
         driver.get(baseUrl);
@@ -29,6 +30,14 @@ public class TestGoogle {
         JSONObject js = (JSONObject) ob;
         findElements = new FindElements(driver);
         findElements.setInputGoogle(js.get("firstName").toString());
+        int length = js.get("age").toString().length();
+        System.out.println(length);
+        if (length < 6) {
+            String newAge = "0" + js.get("age").toString();
+            System.out.println(newAge);
+            js.put("newAge",newAge);
+        }
+
         driver.quit();
     }
 }
