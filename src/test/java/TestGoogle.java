135c963ee098f617a1e90a2b776ca27117894981
dfgr96 <davidfelgomezr@hotmail.com>
2022-10-09 10:18:39 -0500

    Add new chromedriver and test in string

--- src/test/java/TestGoogle.java
+++ src/test/java/TestGoogle.java
@@ -30,12 +30,16 @@ public class TestGoogle {
         JSONObject js = (JSONObject) ob;
         findElements = new FindElements(driver);
         findElements.setInputGoogle(js.get("firstName").toString());
+
         int length = js.get("age").toString().length();
         System.out.println(length);
         if (length < 6) {
             String newAge = "0" + js.get("age").toString();
             System.out.println(newAge);
             js.put("newAge",newAge);
+            System.out.println(newAge.substring(0, 4));
+        }else {
+            System.out.println(js.get("age").toString().substring(0, 4));
         }
 
         driver.quit();
